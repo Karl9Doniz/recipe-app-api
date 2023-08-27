c81cdc9c549a971beb5f449961cc06d62fff8a3d
Admin <admin@Admins-iMac.local>
2023-08-27 20:39:27 +0300

    added recipe image fetaure, configured static files management

--- app/core/models.py
+++ app/core/models.py
@@ -47,12 +47,12 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
 
 
-# def recipe_image_file_path(instance, filename):
-#     """Generate file path for new recipe image."""
-#     ext = os.path.splitext(filename)[1]
-#     filename = f'{uuid.uuid4()}{ext}'
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image."""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
 
-#     return os.path.join('uploads', 'recipe', filename)
+    return os.path.join('uploads', 'recipe', filename)
 
 
 class Recipe(models.Model):
@@ -68,7 +68,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
-    # image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
